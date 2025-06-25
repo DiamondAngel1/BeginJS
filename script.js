d29bf923714e43571a17485a26846e3ce2a5037d
DiamondAngel1 <tararukolga78@gmail.com>
2025-06-25 15:08:38 +0300

    3 HW

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-function age() {
-    const thisYear = 2025;
-    var birthYear = prompt("Введіть Ваш рік народження:");
-    var year = parseInt(birthYear);
+function perimetr() {
+    var side = prompt("Введіть довжину сторони квадрата:");
+    var value = parseFloat(side);
 
-    if (!isNaN(year) && year > 1900 && year <= thisYear) {
-        var age = thisYear - year;
-        alert("Ваш вік: " + age + " років.");
+    if (!isNaN(value) && value > 0) {
+        var perimeter = value * value;
+        alert("Периметр квадрата: " + perimeter);
     } 
     else {
-        alert("Будь ласка, введіть коректний рік народження.");
+        alert("Будь ласка, введіть додатне число.");
     }
 }
